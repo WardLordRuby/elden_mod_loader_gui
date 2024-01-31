960c75a561ecd40615c2be918a9abfedc5509082
Chad Lax <chad2lax@gmail.com>
2024-01-31 00:31:25 -0600

    beginings of new functions

--- src/main.rs
+++ src/main.rs
@@ -5,52 +5,75 @@ slint::include_modules!();
 
 mod ini_parser;
 
-use ini::Ini;
+use ini::{EscapePolicy, Ini, LineSeparator, WriteOption};
 use ini_parser::IniProperty;
-use log::{error, info, warn};
+use log::{debug, error, info, warn};
 use native_dialog::FileDialog;
-use std::path::{Path, PathBuf};
-use std::{env, fs::read_dir, rc::Rc};
+
+use std::{
+    ffi::{OsStr, OsString},
+    fs::{read_dir, rename},
+    path::{Path, PathBuf},
+    {env, rc::Rc},
+};
+
+const INI_WRITE_OPTIONS: WriteOption = WriteOption {
+    escape_policy: EscapePolicy::Nothing,
+    line_separator: LineSeparator::SystemDefault,
+    kv_separator: " = ",
+};
 
 const CONFIG_DIR: &str = "tests\\cfg.ini";
 const DEFAULT_COMMON_DIR: [&str; 4] = ["Program Files (x86)", "Steam", "steamapps", "common"];
-const REQUIRED_GAME_FILES: [&str; 1] = ["test_config.ini"];
+const REQUIRED_GAME_FILES: [&str; 3] = [
+    "eldenring.exe",
+    "oo2core_6_win64.dll",
+    "eossdk-win64-shipping.dll",
+];
 
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
     env_logger::init();
+    let game_dir: PathBuf;
+    {
+        let mut config = match get_cgf(CONFIG_DIR) {
+            Some(ini) => ini,
+            None => {
+                warn!("Ini not found. Creating new Ini");
+                let mut new_ini = Ini::new();
+                //format with comments and placeholders in sections
+                new_ini
+                    .write_to_file_opt(CONFIG_DIR, INI_WRITE_OPTIONS)
+                    .unwrap();
+                attempt_locate_common(&mut new_ini);
+                get_cgf(CONFIG_DIR).unwrap()
+            }
+        };
 
-    let mut config = match get_cgf(CONFIG_DIR) {
-        Some(ini) => ini,
-        None => {
-            warn!("Ini not found. Creating new Ini");
-            let mut new_ini = Ini::new();
-            new_ini.write_to_file(CONFIG_DIR).unwrap();
-            attempt_locate_common(&mut new_ini);
-            get_cgf(CONFIG_DIR).unwrap()
-        }
-    };
-
-    let game_dir: PathBuf = match IniProperty::<PathBuf>::new(&config, Some("paths"), "game_dir") {
-        Ok(ini_property) => {
-            if does_dir_contain(&ini_property.value, &REQUIRED_GAME_FILES) {
-                ini_property.value
-            } else {
+        game_dir = match IniProperty::<PathBuf>::new(&config, Some("paths"), "game_dir") {
+            Ok(ini_property) => {
+                if does_dir_contain(&ini_property.value, &REQUIRED_GAME_FILES) {
+                    info!("Success: \"game_dir\" verified");
+                    ini_property.value
+                } else {
+                    attempt_locate_common(&mut config)
+                }
+            }
+            Err(err) => {
+                error!("{}", err);
                 attempt_locate_common(&mut config)
             }
-        }
-        Err(err) => {
-            error!("{}", err);
-            attempt_locate_common(&mut config)
-        }
-    };
+        };
+    }
 
     ui.set_filepath(game_dir.to_string_lossy().to_string().into());
 
     ui.on_select_file_location({
         let ui_handle = ui.as_weak();
         move || {
+            let mut config = get_cgf(CONFIG_DIR).unwrap();
             let ui = ui_handle.unwrap();
+
             let user_path: Result<String, &'static str> = match get_user_folder(game_dir.as_path())
             {
                 Ok(opt) => match opt {
@@ -63,15 +86,19 @@ fn main() -> Result<(), slint::PlatformError> {
                     Err("Error selecting path")
                 }
             };
+
             match user_path {
                 Ok(path) => {
                     info!("User Selected Path: \"{}\"", &path);
                     ui.set_filepath(path.clone().into());
+                    // include test for \Game\..Before REQUIRED_GAME_FILES
                     match does_dir_contain(Path::new(&path), &REQUIRED_GAME_FILES) {
                         true => {
-                            info!("Sucess: Files found, saving diretory");
+                            info!("Success: Files found, saving diretory");
                             config.with_section(Some("paths")).set("game_dir", path);
-                            config.write_to_file(CONFIG_DIR).unwrap();
+                            config
+                                .write_to_file_opt(CONFIG_DIR, INI_WRITE_OPTIONS)
+                                .unwrap();
                         }
                         false => warn!("Failure: Files not found"),
                     };
@@ -83,12 +110,107 @@ fn main() -> Result<(), slint::PlatformError> {
             }
         }
     });
+
+    ui.on_select_files({
+        move || {
+            let mut config = get_cgf(CONFIG_DIR).unwrap();
+            //only save if valid files are selected
+            let mod_files: Result<Vec<PathBuf>, &'static str> = match get_user_files(Path::new(""))
+            {
+                Ok(opt) => {
+                    save_path_bufs(&mut config, &opt);
+                    Ok(opt)
+                }
+                Err(err) => {
+                    error!("Error selecting path");
+                    error!("{}", err);
+                    Err("Error selecting path")
+                }
+            };
+        }
+    });
+
+    /*
+    let test_files = vec![
+        PathBuf::from("tests\\.a.txt"),
+        PathBuf::from("tests\\.b.txt"),
+        PathBuf::from("tests\\.c.txt"),
+        PathBuf::from("tests\\.d.txt"),
+        PathBuf::from("tests\\.e.txt"),
+    ];
+    match toggle_files(test_files) {
+        Ok(info) => info!("{}", info),
+        Err(err) => error!("{}", err),
+    }
+    */
+
     ui.run()
+}
+
+fn save_path_bufs(config: &mut Ini, files: &[PathBuf]) {
+    let save_paths = files
+        .iter()
+        .map(|path| path.to_string_lossy())
+        .collect::<Vec<_>>()
+        .join("\r\narray[]=");
+    config
+        .with_section(Some("mod-paths"))
+        .set("test_files", format!("mod_files\r\narray[]={}", save_paths));
+    config
+        .write_to_file_opt(CONFIG_DIR, INI_WRITE_OPTIONS)
+        .unwrap();
+    //remove game dir from each path before save
+}
+
+fn toggle_files(file_paths: Vec<PathBuf>) -> Result<&'static str, String> {
+    //change to append .disabled to end of file name
+    fn toggle_name_state(file_name: &OsStr) -> OsString {
+        let mut new_name = file_name.to_string_lossy().to_string();
+        let new_name_clone = new_name.clone();
+        if let Some(first_char) = new_name_clone.chars().next() {
+            match first_char {
+                '.' => new_name = new_name[1..].to_string(),
+                _ => new_name = format!(".{}", new_name),
+            };
+        };
+        OsString::from(new_name)
+    }
+
+    let mut counter: usize = 0;
+    let mut err_msg = String::new();
+    for (index, path) in file_paths.iter().enumerate() {
+        let path_clone = path.clone();
+        let mut new_path = path.clone();
+        if new_path.pop() {
+            if let Some(file_name) = path.file_name() {
+                new_path.push(toggle_name_state(file_name));
+                match rename(&path_clone, &new_path) {
+                    Ok(_) => counter += 1,
+                    Err(err) => error!(
+                        "File: {:?} into {:?} Error: {}",
+                        &path_clone, &new_path, err
+                    ),
+                };
+            };
+        } else {
+            err_msg = format!(
+                "Error: Could not find parent directory at file_path array index: {}",
+                index
+            );
+        }
+    }
+    // if array len == counter then Success output new array with modified names or output bool and save names to ini
+    if counter == file_paths.len() {
+        Ok("Success: All files in array have been renamed")
+    } else {
+        err_msg += "Error: Was not able to rename all files from array[file_paths]";
+        Err(err_msg)
+    }
 }
 
 fn get_cgf(input_file: &str) -> Option<Ini> {
     let path = Path::new(input_file);
-    match Ini::load_from_file(path) {
+    match Ini::load_from_file_noescape(path) {
         Ok(ini) => {
             info!("Config file found at \"{}\"", &input_file);
             Some(ini)
@@ -104,6 +226,12 @@ fn get_user_folder(path: &Path) -> Result<Option<PathBuf>, native_dialog::Error>
     FileDialog::new().set_location(path).show_open_single_dir()
 }
 
+fn get_user_files(path: &Path) -> Result<Vec<PathBuf>, native_dialog::Error> {
+    FileDialog::new()
+        .set_location(path)
+        .show_open_multiple_file()
+}
+
 fn does_dir_contain(path: &Path, list: &[&str]) -> bool {
     match read_dir(path) {
         Ok(entries) => {
@@ -114,14 +242,13 @@ fn does_dir_contain(path: &Path, list: &[&str]) -> bool {
                     .any(|check_file| entry.file_name().to_str() == Some(check_file))
                 {
                     counter += 1;
-                    info!(
+                    debug!(
                         "Found: {:?} in selected directory",
                         &entry.file_name().to_str().unwrap()
                     );
                 }
             }
             if counter == list.len() {
-                info!("All files found in: \"{}\"", path.to_string_lossy());
                 true
             } else {
                 warn!(
@@ -167,7 +294,9 @@ fn attempt_locate_common(config: &mut Ini) -> PathBuf {
         config
             .with_section(Some("paths"))
             .set("common_dir", &common_dir.to_string_lossy().to_string());
-        config.write_to_file(CONFIG_DIR).unwrap();
+        config
+            .write_to_file_opt(CONFIG_DIR, INI_WRITE_OPTIONS)
+            .unwrap();
         info!("default \"common_dir\" wrote to cfg file");
         common_dir
     }
