249651ca85e55bd85842b8159afb2928f8557c97
Chad Lax <chad2lax@gmail.com>
2024-02-03 17:16:51 -0600

    test for new RegMod outward facing all-in-one api

--- tests/test_ini_tools.rs
+++ tests/test_ini_tools.rs
@@ -7,148 +7,97 @@ mod tests {
 
     use elden_mod_loader_gui::{
         get_cgf,
-        ini_tools::{parser::IniProperty, writer::*},
+        ini_tools::{parser::RegMod, writer::*},
     };
     use ini::Ini;
 
     #[test]
-    fn does_path_parse() {
-        let test_path =
-            Path::new("C:\\Program Files (x86)\\Steam\\steamapps\\common\\ELDEN RING\\Game");
-        let test_file = "test_files\\test_path.ini";
-
-        {
-            let mut test_ini = Ini::new();
-            save_path(
-                &mut test_ini,
-                test_file,
-                Some("paths"),
-                "game_dir",
-                test_path,
-            );
-        }
-
-        let config = get_cgf(test_file).unwrap();
-        let parse_test = IniProperty::<PathBuf>::read(&config, Some("paths"), "game_dir")
-            .unwrap()
-            .value;
-        assert_eq!(test_path, parse_test);
-        remove_file(test_file);
-    }
-
-    #[test]
-    fn does_array_parse() {
-        let input_array = vec![
-            PathBuf::from("dinput8.dll"),
-            PathBuf::from("movie\\13000050.bk2"),
-            PathBuf::from("eldenring.exe"),
-            PathBuf::from("EasyAntiCheat\\settings.json"),
+    fn read_write_delete_from_ini() {
+        let test_file = "test_files\\test_collect_mod_data.ini";
+        let mod_1_key = "Unlock The Fps  ";
+        let mod_1_state = false;
+        let mod_2_key = "Skip The Intro";
+        let mod_2_state = true;
+        let mod_1 = vec![
+            PathBuf::from("mods\\UnlockTheFps.dll"),
+            PathBuf::from("mods\\UnlockTheFps\\config.ini"),
         ];
-        let test_file = "test_files\\test_array.ini";
+        let mod_2 = PathBuf::from("mods\\SkipTheIntro.dll");
 
         {
-            // We must save a working game_dir to the same ini before we can parse Vec<PathBuf>
-            // -----------------parser is set up to only parse valid entries------------------
-            // use case for parse Vec<PathBuf> is to keep track of files within game_dir
-            let mut test_ini = Ini::new();
+            let _ = new_cfg(test_file);
+            let mut test_ini: Ini = get_cgf(test_file).unwrap();
+
+            let invalid_format_1 = vec![
+                PathBuf::from("mods\\UnlockTheFps.dll"),
+                PathBuf::from("mods\\UnlockTheFps\\config.ini"),
+            ];
+            let invalid_format_2 = PathBuf::from("mods\\SkipTheIntro.dll");
+
+            // We must save a working game_dir to the same ini before we can parse entries in "mod-files"
+            // ---------------------parser is set up to only parse valid entries---------------------
+            // use case for entries in section "mod-files" is to keep track of files within game_dir
             let game_path =
                 Path::new("C:\\Program Files (x86)\\Steam\\steamapps\\common\\ELDEN RING\\Game");
+
+            save_path_bufs(&mut test_ini, test_file, mod_1_key, &mod_1);
+            save_bool(&mut test_ini, test_file, mod_1_key, mod_1_state);
             save_path(
                 &mut test_ini,
                 test_file,
-                Some("paths"),
-                "game_dir",
-                game_path,
+                Some("mod-files"),
+                mod_2_key,
+                &mod_2,
             );
-        }
-
-        {
-            let mut test_ini = get_cgf(test_file).unwrap();
-            save_path_bufs(&mut test_ini, test_file, "test_files", &input_array);
-        }
-
-        let config = get_cgf(test_file).unwrap();
-        let parse_test =
-            IniProperty::<Vec<PathBuf>>::read(&config, Some("mod-files"), "test_files")
-                .unwrap()
-                .value;
-        assert_eq!(input_array, parse_test);
-        remove_file(test_file);
-    }
-
-    #[test]
-    fn does_bool_parse() {
-        let input_bool_0 = false;
-        let input_bool_1 = true;
-        let test_file = "test_files\\test_bool.ini";
-
-        {
-            let mut test_ini = Ini::new();
-            save_bool(&mut test_ini, test_file, "test_bool_false", input_bool_0);
-            save_bool(&mut test_ini, test_file, "test_bool_true", input_bool_1);
-        }
-
-        let config = get_cgf(test_file).unwrap();
-        let parse_false =
-            IniProperty::<bool>::read(&config, Some("registered-mods"), "test_bool_false")
-                .unwrap()
-                .value;
-        let parse_true =
-            IniProperty::<bool>::read(&config, Some("registered-mods"), "test_bool_true")
-                .unwrap()
-                .value;
-        assert_eq!(input_bool_0, parse_false);
-        assert_eq!(input_bool_1, parse_true);
-        remove_file(test_file);
-    }
-
-    #[test]
-    fn does_array_delete() {
-        let array_to_delete = vec![
-            PathBuf::from("dinput8.dll"),
-            PathBuf::from("movie\\13000050.bk2"),
-            PathBuf::from("eldenring.exe"),
-            PathBuf::from("EasyAntiCheat\\settings.json"),
-        ];
-        let test_file = "test_files\\test_delete_array.ini";
-
-        {
-            let mut test_ini = Ini::new();
-            let game_path =
-                Path::new("C:\\Program Files (x86)\\Steam\\steamapps\\common\\ELDEN RING\\Game");
-            save_path(
-                &mut test_ini,
-                test_file,
-                Some("paths"),
-                "game_dir",
-                game_path,
-            );
-        }
-
-        {
-            let mut test_ini = get_cgf(test_file).unwrap();
+            save_bool(&mut test_ini, test_file, mod_2_key, mod_2_state);
             save_path_bufs(
                 &mut test_ini,
                 test_file,
-                "files_to_delete",
-                &array_to_delete,
+                "no_matching_state_1",
+                &invalid_format_1,
             );
-            save_path_bufs(&mut test_ini, test_file, "do_not_delete", &array_to_delete);
+            save_path(
+                &mut test_ini,
+                test_file,
+                Some("mod-files"),
+                "no_matching_state_2",
+                &invalid_format_2,
+            );
+            save_bool(&mut test_ini, test_file, "no_matching_path", true);
+
+            save_path(
+                &mut test_ini,
+                test_file,
+                Some("paths"),
+                "game_dir",
+                game_path,
+            );
         }
 
-        let _ = IniProperty::<PathBuf>::remove_array(test_file, "files_to_delete");
-        let test_ini = get_cgf(test_file).unwrap();
-        let array_key_deleted = test_ini
-            .section(Some("mod-files"))
-            .unwrap()
-            .contains_key("test_files");
-        let other_array = test_ini
-            .section(Some("mod-files"))
-            .unwrap()
-            .contains_key("do_not_delete");
+        // -------------------------------------sync_keys runs from inside RegMod::collect()------------------------------------------------
+        // ----this deletes any keys that do not have a matching state eg. (key has state but no files, or key has files but no state)-----
+        // this tests delete_entry && delete_array in this case we delete "no_matching_path", "no_matching_state_1", and "no_matching_state_2"
+        let registered_mods = RegMod::collect(test_file);
+        assert_eq!(registered_mods.len(), 2);
 
-        assert!(!array_key_deleted);
-        assert!(other_array);
-        remove_file(test_file);
+        // Tests name format is correct
+        let reg_mod_1: &RegMod = registered_mods
+            .iter()
+            .find(|data| data.name == mod_1_key.trim())
+            .unwrap();
+        let reg_mod_2: &RegMod = registered_mods
+            .iter()
+            .find(|data| data.name == mod_2_key.trim())
+            .unwrap();
+
+        // Tests if PathBuf and Vec<PathBuf> was parsed correctly
+        assert_eq!(mod_1, reg_mod_1.files);
+        assert_eq!(mod_2, reg_mod_2.files[0]);
+
+        // Tests if bool was parsed correctly
+        assert_eq!(mod_1_state, reg_mod_1.state);
+        assert_eq!(mod_2_state, reg_mod_2.state);
+
+        let _ = remove_file(test_file);
     }
 }
