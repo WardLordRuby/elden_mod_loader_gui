0700b90c2258d9829fb3fa0004b419dcbd1ad386
Chad Lax <chad2lax@gmail.com>
2024-02-03 17:15:04 -0600

    finished parser

--- src/ini_tools/parser.rs
+++ src/ini_tools/parser.rs
@@ -2,10 +2,14 @@ use ini::Ini;
 use log::{error, info, warn};
 
 use std::{
-    fs::{read_to_string, write},
-    io,
+    collections::HashMap,
     path::{Path, PathBuf},
     str::FromStr,
+};
+
+use crate::{
+    get_cgf,
+    ini_tools::writer::{remove_array, remove_entry},
 };
 
 pub struct IniProperty<T: ValueType> {
@@ -34,7 +38,16 @@ impl ValueType for bool {
 impl ValueType for PathBuf {
     fn parse_str(ini: &Ini, section: Option<&str>, key: &str) -> Option<Self> {
         let ini_path = PathBuf::from(ini.get_from(section, key).unwrap());
-        if validate_path(&ini_path) {
+        if !ini_path.has_root() {
+            let game_dir = IniProperty::<PathBuf>::read(ini, Some("paths"), "game_dir")
+                .unwrap()
+                .value;
+            if validate_path(&game_dir.join(&ini_path)) {
+                Some(ini_path)
+            } else {
+                None
+            }
+        } else if validate_path(&ini_path) {
             Some(ini_path)
         } else {
             None
@@ -76,7 +89,8 @@ impl ValueType for Vec<PathBuf> {
 
 impl<T: ValueType> IniProperty<T> {
     pub fn read(ini: &Ini, section: Option<&str>, key: &str) -> Result<IniProperty<T>, String> {
-        match IniProperty::is_valid(ini, section, key) {
+        let format_key = key.replace(' ', "_");
+        match IniProperty::is_valid(ini, section, &format_key) {
             Some(value) => {
                 info!("Success: read \"{}\" from ini", key);
                 Ok(IniProperty {
@@ -91,31 +105,6 @@ impl<T: ValueType> IniProperty<T> {
                 key
             )),
         }
-    }
-
-    pub fn remove_array(path: &str, key: &str) -> io::Result<()> {
-        let content = read_to_string(path)?;
-
-        let mut skip_next_line = false;
-        let mut key_found = false;
-
-        let mut filter_lines = |line: &str| {
-            if key_found && !line.starts_with("array[]") {
-                skip_next_line = false;
-                key_found = false;
-            }
-            if line.starts_with(key) {
-                skip_next_line = true;
-                key_found = true;
-            }
-            !skip_next_line
-        };
-
-        let lines: Vec<&str> = content.lines().filter(|&line| filter_lines(line)).collect();
-
-        write(path, lines.join("\r\n"))?;
-
-        Ok(())
     }
 
     fn is_valid(ini: &Ini, section: Option<&str>, key: &str) -> Option<T> {
@@ -151,3 +140,85 @@ fn validate_path(path: &Path) -> bool {
         }
     }
 }
+
+pub struct RegMod {
+    pub name: String,
+    pub state: bool,
+    pub files: Vec<PathBuf>,
+}
+
+impl RegMod {
+    pub fn collect(path: &str) -> Vec<Self> {
+        fn sync_keys(path: &str) {
+            let mut ini = get_cgf(path).unwrap();
+            let mod_state_data = ini.section(Some("registered-mods")).unwrap().clone();
+            let mod_files_data = ini.section(Some("mod-files")).unwrap().clone();
+            let state_keys: Vec<&str> = mod_state_data.iter().map(|(k, _)| k).collect();
+            let file_keys: Vec<&str> = mod_files_data
+                .iter()
+                .filter_map(|(k, _)| if k != "array[]" { Some(k) } else { None })
+                .collect();
+            for key in state_keys.iter().filter(|&&k| !file_keys.contains(&k)) {
+                remove_entry(&mut ini, path, Some("registered-mods"), key).unwrap()
+            }
+            for key in file_keys.iter().filter(|&&k| !state_keys.contains(&k)) {
+                if ini.get_from(Some("mod-files"), key).unwrap() == "array" {
+                    remove_array(path, key);
+                    ini = get_cgf(path).unwrap();
+                } else {
+                    remove_entry(&mut ini, path, Some("mod-files"), key).unwrap()
+                }
+            }
+        }
+        sync_keys(path);
+        let ini = get_cgf(path).unwrap();
+        let mod_state_data = ini.section(Some("registered-mods")).unwrap();
+        let mod_files_data = ini.section(Some("mod-files")).unwrap();
+        let state_data: HashMap<String, bool> = mod_state_data
+            .iter()
+            .map(|(key, _)| {
+                (
+                    key.replace('_', " ").to_string(),
+                    IniProperty::<bool>::read(&ini, Some("registered-mods"), key)
+                        .unwrap()
+                        .value,
+                )
+            })
+            .collect();
+        let file_data: HashMap<String, Vec<PathBuf>> = mod_files_data
+            .iter()
+            .filter_map(|(key, _)| {
+                if key != "array[]" {
+                    Some((
+                        key.replace('_', " ").to_string(),
+                        if ini.get_from(Some("mod-files"), key).unwrap() == "array" {
+                            IniProperty::<Vec<PathBuf>>::read(&ini, Some("mod-files"), key)
+                                .unwrap()
+                                .value
+                        } else {
+                            vec![
+                                IniProperty::<PathBuf>::read(&ini, Some("mod-files"), key)
+                                    .unwrap()
+                                    .value,
+                            ]
+                        },
+                    ))
+                } else {
+                    None
+                }
+            })
+            .collect();
+        let mut reg_mods = Vec::new();
+
+        for (name, state) in state_data {
+            let files = file_data.get(&name).cloned().unwrap_or_default();
+            reg_mods.push(RegMod { name, state, files });
+        }
+
+        reg_mods
+    }
+}
+// make sure keys in Some("registered-mods") are also in Some("mod-files")
+//      remove entry if doesnt match
+// replace _ with space in name
+// if value == array then multiple_files true
