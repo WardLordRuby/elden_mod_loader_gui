29892dc7ce941406a0e90ddc50151a3cbea985d5
Chad Lax <chad2lax@gmail.com>
2024-02-10 23:05:29 -0600

    New benchmark for data collection

---
+++ benches/data_collection_benchmark.rs
@@ -0,0 +1,51 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+use elden_mod_loader_gui::ini_tools::{parser::RegMod, writer::*};
+use rand::{distributions::Alphanumeric, Rng};
+use std::{fs::remove_file, path::PathBuf};
+
+fn populate_non_valid_ini(len: u64, file: &str) {
+    let _ = new_cfg(file);
+    for i in 0..len {
+        let key = format!("key_{}", i);
+        let bool_value = rand::thread_rng().gen_bool(0.5);
+        let paths = generate_test_paths();
+
+        let _ = save_bool(BENCH_TEST_FILE, &key, bool_value);
+        if paths.len() > 1 {
+            let _ = save_path_bufs(BENCH_TEST_FILE, &key, &paths);
+        } else {
+            let _ = save_path(BENCH_TEST_FILE, Some("mod-files"), &key, paths[0].as_path());
+        }
+    }
+}
+
+fn generate_test_paths() -> Vec<PathBuf> {
+    let num_paths = rand::thread_rng().gen_range(1..5);
+    (0..num_paths)
+        .map(|_| {
+            PathBuf::from(
+                rand::thread_rng()
+                    .sample_iter(&Alphanumeric)
+                    .take(10)
+                    .map(char::from)
+                    .collect::<String>(),
+            )
+        })
+        .collect()
+}
+
+const BENCH_TEST_FILE: &str = "test_files\\benchmark_test.ini";
+
+fn data_collection_benchmark(c: &mut Criterion) {
+    let num_entries: u64 = 25;
+    populate_non_valid_ini(num_entries, BENCH_TEST_FILE);
+
+    c.bench_function("data_collection", |b| {
+        b.iter(|| black_box(RegMod::collect(BENCH_TEST_FILE, true)));
+    });
+    let _ = remove_file(BENCH_TEST_FILE);
+}
+
+criterion_group!(benches, data_collection_benchmark);
+criterion_main!(benches);
