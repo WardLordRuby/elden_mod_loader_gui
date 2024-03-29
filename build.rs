5a41a27e511ea622d0cf33e626d40c502b7f14f9
Chad Lax <chad2lax@gmail.com>
2024-03-29 03:27:01 -0500

    Added app icon and metadata

--- build.rs
+++ build.rs
@@ -1,3 +1,19 @@
+#[cfg(windows)]
+extern crate winresource;
+
+/// `MAJOR << 48 | MINOR << 32 | PATCH << 16 | RELEASE`
+const MAJOR: u64 = 0;
+const MINOR: u64 = 9;
+const PATCH: u64 = 0;
+const RELEASE: u64 = 2;
+
 fn main() {
-    slint_build::compile("ui/appwindow.slint").unwrap();
+    if cfg!(target_os = "windows") {
+        slint_build::compile("ui/appwindow.slint").unwrap();
+        let mut res = winresource::WindowsResource::new();
+        res.set_icon("ui/assets/EML_icon.ico");
+        let version: u64 = (MAJOR << 48) | (MINOR << 32) | (PATCH << 16) | RELEASE;
+        res.set_version_info(winresource::VersionInfo::FILEVERSION, version);
+        res.compile().unwrap();
+    }
 }
