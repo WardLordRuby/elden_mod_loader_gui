613862ced2bdc5872b77e5be7b12eeaa2a70bb15
Chad Lax <chad2lax@gmail.com>
2024-04-05 23:45:43 -0500

    Switched all '_' for '-' in the front end

--- build.rs
+++ build.rs
@@ -11,7 +11,7 @@ fn main() {
     if cfg!(target_os = "windows") {
         slint_build::compile("ui/appwindow.slint").unwrap();
         let mut res = winresource::WindowsResource::new();
-        res.set_icon("ui/assets/EML_icon.ico");
+        res.set_icon("ui/assets/EML-icon.ico");
         let version: u64 = (MAJOR << 48) | (MINOR << 32) | (PATCH << 16) | RELEASE;
         res.set_version_info(winresource::VersionInfo::FILEVERSION, version);
         res.compile().unwrap();
