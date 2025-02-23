611e71828c75e8532125e0e65b06030d18fd5c99
WardLordRuby <chad2lax@gmail.com>
2025-02-23 05:05:08 -0600

    Bump rand to 0.9

--- benches/data_collection_benchmark.rs
+++ benches/data_collection_benchmark.rs
@@ -7,7 +7,7 @@ use elden_mod_loader_gui::{
     },
     INI_NAME, INI_SECTIONS,
 };
-use rand::{distributions::Alphanumeric, Rng};
+use rand::{distr::Alphanumeric, Rng};
 use std::{
     fs::remove_file,
     path::{Path, PathBuf},
@@ -19,7 +19,7 @@ fn populate_non_valid_ini(len: u32, file: &Path) {
     new_cfg(file).unwrap();
     for i in 0..len {
         let key = format!("key_{}", i);
-        let bool_value = rand::thread_rng().gen_bool(0.5);
+        let bool_value = rand::rng().random_bool(0.5);
         let paths = generate_test_paths();
         let path_refs = paths.iter().map(|p| p.as_path()).collect::<Vec<_>>();
 
@@ -33,11 +33,11 @@ fn populate_non_valid_ini(len: u32, file: &Path) {
 }
 
 fn generate_test_paths() -> Vec<PathBuf> {
-    let num_paths = rand::thread_rng().gen_range(1..5);
+    let num_paths = rand::rng().random_range(1..5);
     (0..num_paths)
         .map(|_| {
             PathBuf::from(
-                rand::thread_rng()
+                rand::rng()
                     .sample_iter(&Alphanumeric)
                     .take(10)
                     .map(char::from)
